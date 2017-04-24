0cdde911e46eb1bd2634cdb9ef4a3d38c4f90abe
hyunjinchoe <hyunjinchoe@hotmail.com>
2017-04-23 21:15:40 -0700

    Calendar - final

--- calendar/light/static/process.php
+++ calendar/light/static/process.php
@@ -1,12 +1,15 @@
 ﻿<?php
 // Information to be modified
-$to_email = "support@zozothemes.com"; // email address to which the form data will be sent
+$to_email = "jchoe@genemod.net"; // email address to which the form data will be sent
+$to_email2 = "jlee@genemod.net";
+$to_email3 = "jsugianto@genemod.net";
 $subject = "Contact Request"; // subject of the email that is sent
 $thanks_page = "index.html"; // path to the thank you page following successful form submission
 $contact_page = "index.html"; // path to the HTML contact page where the form appears
 
 
 $nam = strip_tags($_POST["contact_name"]);
+$num = strip_tags($_POST["contact_num"]);
 $ema = strip_tags($_POST["contact_email"]);
 $com = strip_tags($_POST["contact_message"]);
 
@@ -17,12 +20,15 @@ $headers .= "Reply-To: ".$ema."\r\n";
 
 $email_body = 
 	"<strong>From: </strong>" . $nam . "<br />
+	<strong>Phone Number: </strong>" . $num . "<br />	
 	<strong>Email: </strong>" . $ema . "<br />	
 	<strong>Message: </strong>" . $com;	
 
 // Assuming there's no error, send the email and redirect to Thank You page
 	
-if( mail($to_email, $subject, $email_body, $headers) ) {	
+if( mail($to_email, $subject, $email_body, $headers) && 
+    mail($to_email2, $subject, $email_body, $headers) &&
+    mail($to_email3, $subject, $email_body, $headers) ) {	
 	echo '<i class="glyphicon glyphicon-ok"></i> Thank you ' .$nam. '. Your Email was successfully sent!';
 } else {	
 	echo '<i class="glyphicon glyphicon-remove"></i> Sorry ' .$nam. '. Your Email was not sent. Resubmit form again Please..';
